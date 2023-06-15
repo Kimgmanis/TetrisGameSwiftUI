6e94b558faa0ee4b60275fc701f212f362995762
Kim K <kimalexanderkeiser@gmail.com>
2023-06-15 17:31:45 +0200

    Initial Commit

---
+++ TetrisGameSwiftUI/TetrisGameSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TetrisGameSwiftUIApp.swift
+//  TetrisGameSwiftUI
+//
+//  Created by Kim Keiser on 15/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct TetrisGameSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
